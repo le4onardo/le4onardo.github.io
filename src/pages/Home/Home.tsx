ac4cd9c07846005e8a517677106d1c96c3904113
le4o <leonardo_jlo@hotmail.com>
2022-08-02 21:31:05 -0400

    Glitcher use in home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,22 @@
 import MainLayout from '../../components/templates/MainLayout/MainLayout';
+import Glitcher from '../../components/atoms/GlitcherHOC';
 import './Home.css';
 
 const Home = () => {
   return (
     <MainLayout classProps='home'>
       <div className='container'>
-        <div className='container-title'>
-          THIS IS A TITLE, AND SOME MORE TEXT
-        </div>
+        <Glitcher
+          classProps='container-title'
+          text='Welcome, to my little place on the web :)'
+          intensity={0.5}
+          colorIntensity={0.4}
+        />
         <p className='container-description'>
           This is supossed to be a large description, something about me I
-          guess, lets just prin dummy chars: sldgijls lsdfj sld sdkjf lsdk
+          guess, lets just prin dummy chars: sldgijls lsdfj sld sdkjf lsdk This
+          is supossed to be a large description, something about me I guess,
+          lets just prin dummy chars: sldgijls lsdfj sld sdkjf lsdk This is
         </p>
       </div>
     </MainLayout>
