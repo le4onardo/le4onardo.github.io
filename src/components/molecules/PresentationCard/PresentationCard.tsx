2b91f44a1a56e1095340bbec39db46387308a54d
Leonardo Julio Rios Aliaga <44624540+le4onardo@users.noreply.github.com>
2023-08-28 05:37:36 +0000

    add link in presentation card

--- src/components/molecules/PresentationCard/PresentationCard.tsx
+++ src/components/molecules/PresentationCard/PresentationCard.tsx
@@ -1,22 +1,25 @@
+import { Link } from 'react-router-dom';
 import './PresentationCard.css';
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
     title: string,
     description: string,
     writeDate: Date,
-    readTime: number // minutes
+    readTime: number, // minutes
+    redirectTo: string
 }
 
-const PresentationCard: React.FC<Props> = ({ title, description, writeDate, readTime, ...props }: Props) => {
+const PresentationCard: React.FC<Props> = ({ title, description, writeDate, readTime, redirectTo, ...props }: Props) => {
     const formatedDate = writeDate.toISOString().split('T')[0]
     const readTimeLabel = readTime + (readTime > 1 ? " mins" : " min") + " read";
 
     return <div className='presentation-card' {...props}>
-        <span className='card-title'> {title} </span>
-        <p className='card-description'> {description} </p>
+        <Link to={`/${redirectTo}`} >
+            <span className='card-title'> {title} </span>
+            <p className='card-description'> {description} </p>
+        </Link>
         <div className='card-info'>
             <p>{formatedDate}</p>
-
             <p>{readTimeLabel} </p>
         </div>
     </div>
