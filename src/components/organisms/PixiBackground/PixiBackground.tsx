fb2e0d1dcb84b4ee3669fda64536a72ea91209ac
Leonardo Julio Rios Aliaga <44624540+le4onardo@users.noreply.github.com>
2023-09-04 19:38:43 +0000

    pixi background initial refactor

--- src/components/organisms/PixiBackground/PixiBackground.tsx
+++ src/components/organisms/PixiBackground/PixiBackground.tsx
@@ -1,145 +1,178 @@
 import { useRef, useEffect, memo } from 'react';
 import './PixiBackground.css';
-import { Application, Sprite, Texture } from 'pixi.js';
-import { assets } from '../../../assets';
+import { Application, Sprite, Texture, Ticker } from 'pixi.js';
 import AsciiFilter from 'pixi-ascii';
-import { CRTFilter } from 'pixi-filters';
-import GlitchEmisorFilter from '../../atoms/GlitchEmitterFilter/GlitchEmisorFilter';
+import { CRTFilter, GlitchFilter } from 'pixi-filters';
+import GlitchEmisorFilter from '../../../utils/pixi-utils/GlitchEmitterFilter/GlitchEmisorFilter';
+import { AssestType } from '../../../utils/data';
 
-// TODO: check pixi react library 
-const PixiBackground = () => {
+
+async function fetchVideoInMedia(url: string) {
+  const media = document.createElement('video') as HTMLVideoElement;
+  media.crossOrigin = '';
+  media.src = url;
+  media.muted = true;
+  media.autoplay = true;
+  media.loop = true;
+
+  const promise = new Promise((resolve, reject) => {
+    media.onloadeddata = function () {
+      resolve(media);
+    };
+    media.onerror = function () {
+      reject('load error');
+    };
+  });
+
+  media.load();
+  console.log('load requested');
+  return promise;
+}
+
+async function resizeSprite(newWidth: number, newHeight: number, sprite: Sprite) {
+  // INFO: changing constant changes sprite size keeping aspect ratio
+  // sprite.scale.x = (canvasEl.current!.clientWidth / texture.width) * 0.75;
+  // sprite.scale.y = (canvasEl.current!.clientWidth / texture.width) * 0.75;
+  sprite.scale.x = ((newWidth + 1) / sprite.texture.width) * 0.75;
+  sprite.scale.y = ((newWidth + 1) / sprite.texture.width) * 0.75;
+  console.log('dimensions', newWidth, newHeight, sprite.texture.width);
+
+  // INFO: height correction needed for cut chars on some heights
+  const intHeight = Math.floor(sprite.height);
+  if (intHeight % 4 !== 0) {
+    sprite.height = intHeight + 4 - (intHeight % 4);
+  }
+  console.log(sprite.height);
+  // INFO: centers media in sprite
+  sprite.x = (newWidth - sprite.width) / 2;
+  sprite.y = (newHeight - sprite.height) / 2;
+}
+
+interface Props {
+  height: number;
+  width: number;
+  videoData: AssestType;
+  asciiSize?: number;
+  ticker?: (glitchFilter: GlitchEmisorFilter, crtFilter: CRTFilter, asciiFilter: AsciiFilter) => void,
+}
+
+const MAX_GLITCH_INDEX = 0.3
+
+// TODO: check pixi react library
+const PixiBackground: React.FC<Props> = ({ height, width, videoData, ticker, asciiSize = 1 }: Props) => {
   const canvasEl = useRef<HTMLCanvasElement>(null);
-  const spriteHeight = 700;
-  const spriteWidth = 1400;
+  const sprite = useRef<Sprite>();
+  const crtFilter = useRef<CRTFilter>();
+  const asciiFilter = useRef<AsciiFilter>();
+  const glitchFilter = useRef<GlitchEmisorFilter>();
+  const pixiApp = useRef<Application>();
+  const loading = useRef<boolean>(false);
+
+  const loadVideoInCanvas = async () => {
+    try {
+      loading.current = true;
+      const media = await fetchVideoInMedia(videoData.url) as HTMLVideoElement;
+      const texture = Texture.from(media);
+      sprite.current!.texture = texture;
+      resizeSprite(width, height, sprite.current!);
+
+      crtFilter.current!.vignetting = videoData.crtVignetting;
+      crtFilter.current!.vignettingAlpha =
+        videoData.crtVignettingAlpha
+      crtFilter.current!.vignettingBlur = videoData.crtVignettingBlur;
+    } catch (error) {
+      console.log('video load failed', videoData.url);
+    } finally {
+      setTimeout(() => (loading.current = false), 1000);
+    }
+  }
 
   useEffect(() => {
-    const app = new Application({
+    pixiApp.current = new Application({
       view: canvasEl.current!,
       resolution: window.devicePixelRatio || 1,
       autoDensity: true,
       // backgroundColor: 0x10101b,
-      // INFO: width correction needed for cut chars on some widths
-      width: spriteWidth + 1,
-      height: spriteHeight
+      // TODO: "width" correction needed for cut chars on some widths
+      width: width,
+      height: height
     });
-    let sprite: Sprite;
-    let glitch: GlitchEmisorFilter;
-    let loading = false;
-    let glitchCounter = 0;
-    let crtFilter: CRTFilter;
 
-    async function loadRandomVideo(
-      sprite: Sprite,
-      crtFilter: CRTFilter,
-      index?: number
-    ) {
-      const randomIndex =
-        index === undefined ? Math.floor(Math.random() * assets.length) : index;
-      const media = document.createElement('video') as HTMLVideoElement;
-      media.crossOrigin = '';
-      media.src = assets[randomIndex].url;
-      media.muted = true;
-      media.autoplay = true;
-      media.loop = true;
-      media.onloadeddata = function () {
-        crtFilter.vignetting = assets[randomIndex].crtConfig.vignetting;
-        crtFilter.vignettingAlpha =
-          assets[randomIndex].crtConfig.vignettingAlpha;
-        crtFilter.vignettingBlur = assets[randomIndex].crtConfig.vignettingBlur;
-        const texture = Texture.from(media);
-        sprite.texture = texture;
-
-        // INFO: changing constant changes sprite size keeping aspect ratio
-        sprite.scale.x = (canvasEl.current!.clientWidth / texture.width) * 0.75;
-        sprite.scale.y = (canvasEl.current!.clientWidth / texture.width) * 0.75;
-        console.log(sprite.height);
-
-        // INFO: height correction needed for cut chars on some heights
-        const intHeight = Math.floor(sprite.height);
-        if (intHeight % 4 !== 0) {
-          sprite.height = intHeight + 4 - (intHeight % 4);
-        }
-        console.log(sprite.height);
-        // INFO: centers media in sprite
-        sprite.x = (spriteWidth - sprite.width) / 2;
-        sprite.y = (spriteHeight - sprite.height) / 2;
-        setTimeout(() => (loading = false), 1000);
-        console.log('video loaded', assets[randomIndex].url);
-      };
-      media.onerror = function () {
-        setTimeout(() => (loading = false), 1000);
-        console.log('video load failed', assets[randomIndex].url);
-      };
-      console.log('load requested');
-      media.load();
-    }
-    function main() {
+    async function main() {
       const backSprite = new Sprite();
+      sprite.current = new Sprite();
+      crtFilter.current = new CRTFilter();
+      asciiFilter.current = new AsciiFilter();
+      glitchFilter.current = new GlitchEmisorFilter();
+
       backSprite.width = canvasEl.current!.clientWidth;
       backSprite.height = canvasEl.current!.clientHeight;
-      app.stage.addChild(backSprite);
-      const ascii = new AsciiFilter();
-      glitch = new GlitchEmisorFilter({
-        offset: 100,
-        slices: 100
-      });
-      glitch.intensity = 0;
-      ascii.size = 5;
 
-      setTimeout(() => (ascii.size = 4), 2000);
-      setTimeout(() => (ascii.size = 3), 4000);
-      setTimeout(() => (ascii.size = 2), 6000);
-      setTimeout(() => (ascii.size = 1), 8000);
-      //ascii.backgroundColor = [0.062, 0.062, 0.105, 1.0];
-      crtFilter = new CRTFilter({
-        noise: 0,
-        curvature: 0,
-        lineWidth: 0,
-        lineContrast: 0,
-        seed: 0,
-        time: 0
-      });
+      crtFilter.current.noise = 0;
+      crtFilter.current.curvature = 0;
+      crtFilter.current.lineWidth = 0;
+      crtFilter.current.lineContrast = 0;
+      crtFilter.current.seed = 0;
+      crtFilter.current.time = 0;
 
-      ascii.charIndexes = [
+      glitchFilter.current.intensity = 0;
+      glitchFilter.current.offset = 100;
+      glitchFilter.current.slices = 100;
+
+      asciiFilter.current.charIndexes = [
         0, 96, 34, 94, 92, 93, 111, 110, 51, 98, 38, 72, 65, 66, 64, 48
       ];
-      sprite = new Sprite();
-      app.stage.addChild(sprite);
+      //ascii.backgroundColor = [0.062, 0.062, 0.105, 1.0];
 
-      sprite.filters = [crtFilter, ascii];
-      app.stage.filters = [glitch];
-      setTimeout(() => glitch.startGlitch(), 100);
-      loadRandomVideo(sprite, crtFilter, 19);
+      pixiApp.current!.stage.addChild(backSprite);
+      pixiApp.current!.stage.addChild(sprite.current);
+
+      sprite.current.filters = [crtFilter.current, asciiFilter.current];
+      pixiApp.current!.stage.filters = [glitchFilter.current];
+
+      setTimeout(() => glitchFilter.current!.startGlitch(), 100);
+      console.log('main executed');
     }
 
-    document.onmousemove = (event: MouseEvent) => {
+    const onMouseMove = (event: MouseEvent) => {
       const x = event.movementX;
       const y = event.movementY;
       const increment = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-      console.log(x, y, increment);
-      glitch.intensity = Math.min(
-        glitch.intensity + increment / 6000, //* (0.5 - increment),
-        0.3
+
+      glitchFilter.current!.intensity = Math.min(
+        glitchFilter.current!.intensity + increment / 9000, //* (0.5 - increment),
+        MAX_GLITCH_INDEX
       );
     };
-    app.loader.load(main);
-    app.ticker.maxFPS = 30;
-    app.ticker.add((delta) => {
-      if (loading) {
-        glitch.intensity = 0.3;
+    document.onmousemove = onMouseMove;
+
+    pixiApp.current.loader.load(main);
+    pixiApp.current.ticker.maxFPS = 30;
+    pixiApp.current.ticker.add(async (_delta) => {
+      if (loading.current) {
+        glitchFilter.current!.intensity = MAX_GLITCH_INDEX;
         return;
       }
-      if (glitch.intensity === 0.3) glitchCounter++;
-      else glitchCounter = Math.max(glitchCounter - 1, 0);
-      if (glitchCounter >= 30) {
-        glitchCounter = 0;
-        loading = true;
-        console.log('start async load', glitchCounter, delta);
-        loadRandomVideo(sprite, crtFilter);
-      }
-      glitch.intensity = Math.max(glitch.intensity - 0.005, 0);
+      ticker && ticker(glitchFilter.current!, crtFilter.current!, asciiFilter.current!);
+
+      // Smooth glitch reduction effect over time
+      glitchFilter.current!.intensity = Math.max(glitchFilter.current!.intensity - 0.005, 0);
     });
-  });
+
+    return () => {
+      document.removeEventListener('mousemove', onMouseMove);
+      pixiApp.current!.destroy(false, true);
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log('new Video', videoData.url);
+    loadVideoInCanvas();
+  }, [videoData])
+
+  useEffect(() => {
+    asciiFilter.current!.size = asciiSize;
+  }, [asciiSize])
 
   return <canvas className='pixi-background' ref={canvasEl} />;
 };
