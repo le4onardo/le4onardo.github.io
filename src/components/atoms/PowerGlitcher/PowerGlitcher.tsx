885b90557b3c50f31cbdee903f055952771dc89b
Leonardo Julio Rios Aliaga <44624540+le4onardo@users.noreply.github.com>
2024-08-05 20:56:10 +0000

    legacy power glitch

--- src/components/atoms/PowerGlitcher/PowerGlitcher.tsx
+++ src/components/atoms/PowerGlitcher/PowerGlitcher.tsx
@@ -1,20 +1,50 @@
 import { useRef, useCallback, useEffect } from 'react';
 import { useGlitch, } from 'react-powerglitch';
-import { PowerGlitchOptions, RecursivePartial, PlayModes } from 'powerglitch'
+import { PowerGlitchOptions, PlayModes } from 'powerglitch'
 import './PowerGlitcher.css';
 
-interface Props {
-    // -- PowerGlitch props
+interface PowerGlitchProps {
     playMode?: PlayModes
     duration?: number | undefined
     iterations?: number | undefined
     timeStart?: number | undefined
     timeEnd?: number | undefined
-    // -- 
-
+}
+interface Props extends PowerGlitchProps {
     classProps?: string;
+    colors?: string[];
     children: React.ReactNode;
 }
+
+const getDefaultProps = (props: PowerGlitchProps): PowerGlitchOptions => ({
+    createContainers: true,
+    playMode: props.playMode || "hover",
+    hideOverflow: false,
+    timing: {
+        duration: props.duration || 150,
+        iterations: props.iterations || 1,
+        easing: 'linear',
+    },
+
+    glitchTimeSpan: {
+        start: props.timeStart || 0,
+        end: props.timeEnd || 1
+    },
+
+    shake: {
+        velocity: 20,
+        amplitudeX: 0.10,
+        amplitudeY: 0.05
+    },
+    slice: {
+        count: 3,
+        velocity: 30,
+        minHeight: 0.1,
+        maxHeight: 0.6,
+        hueRotate: true
+    },
+    pulse: false,
+})
 
 // TODO: make new glitch props update the glitcher
 const GlitcherHOC: React.FC<Props> = ({
@@ -23,37 +53,21 @@ const GlitcherHOC: React.FC<Props> = ({
     iterations,
     timeStart,
     timeEnd,
-
     classProps = '',
+    colors = ['red', 'green', 'blue'],
     children
 }: Props) => {
     const ref = useRef<HTMLElement>();
-    const glitch = useGlitch({
-        createContainers: true,
-        playMode: playMode || "hover",
-        hideOverflow: false,
-        timing: {
-            duration: duration || 150,
-            iterations: iterations || 1,
-        },
-        glitchTimeSpan: {
-            start: timeStart || 0,
-            end: timeEnd || 1
-        },
-        shake: {
-            velocity: 20,
-            amplitudeX: 0.10,
-            amplitudeY: 0.05
-        },
-        slice: {
-            count: 3,
-            velocity: 30,
-            minHeight: 0.1,
-            maxHeight: 0.6,
-            hueRotate: true
-        },
-        pulse: false,
-    });
+    const glitch = useGlitch(
+        getDefaultProps({
+            playMode,
+            duration,
+            iterations,
+            timeStart,
+            timeEnd
+        })
+    );
+
     const refCallback = useCallback((node: HTMLElement | null) => {
         if (!node) {
             return;
@@ -66,21 +80,29 @@ const GlitcherHOC: React.FC<Props> = ({
         // Omitting 0 because is the actual react element displayed on screen
         for (let i = 1; i < glitchElements.length; i++) {
             const el = glitchElements[i] as HTMLElement;
-            if (i % 3 === 0) el.style.setProperty('color', 'red', 'important');
-            if (i % 3 === 1) el.style.setProperty('color', 'green', 'important');
-            if (i % 3 === 2) el.style.setProperty('color', 'blue', 'important');
-            el.classList.add('glitch-layer')
+            el.style.setProperty('color', colors[i % colors.length], 'important');
+            el.classList.add('glitch-layer');
         }
-    }, [])
+    }, [glitch?.ref])
 
     useEffect(() => {
-        const glitchContainer = ref.current!.parentElement!.parentElement
+        const glitchContainer = ref.current?.parentElement?.parentElement
         glitchContainer && classProps && glitchContainer.classList.add(classProps)
 
         return () => {
             glitchContainer && classProps && glitchContainer.classList.remove(classProps);
         }
     }, [classProps])
+
+    useEffect(() => {
+        glitch.setOptions(getDefaultProps({
+            playMode, duration, iterations, timeStart, timeEnd
+        }));
+    }, [playMode,
+        duration,
+        iterations,
+        timeStart,
+        timeEnd])
 
     return (
         <div ref={refCallback}>
@@ -90,3 +112,6 @@ const GlitcherHOC: React.FC<Props> = ({
 };
 
 export default GlitcherHOC;
+
+
+
