fffe457a3b645e3a6b243148fd22e8406c9eb213
Leonardo Julio Rios Aliaga <44624540+le4onardo@users.noreply.github.com>
2024-08-12 03:35:51 +0000

    glitch code improvements

--- src/components/templates/MainLayout/MainLayout.tsx
+++ src/components/templates/MainLayout/MainLayout.tsx
@@ -1,10 +1,9 @@
-import { useEffect, useState, useCallback, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import PixiBackground from '../../organisms/PixiBackground/PixiBackground';
 import NavigationBar from '../../organisms/NavigationBar/NavigationBar';
 import './MainLayout.css';
 import { AssestType, assets } from '../../../utils/data';
 import GlitchEmisorFilter from '../../../utils/pixi-utils/GlitchEmitterFilter/GlitchEmisorFilter';
-import { CRTFilter } from 'pixi-filters';
 
 interface Props {
   children: React.ReactNode;
@@ -12,34 +11,49 @@ interface Props {
 }
 
 const getRandomVideo = () => assets[Math.floor(Math.random() * assets.length)];
+const MAX_GLITCH_INDEX = 0.3
+const GLITCH_DEGRADING_FACTOR = 0.005
+const NEXT_VIDEO_THRESHOLD = 20
 
 const MainLayout: React.FC<Props> = ({ children, classProps }: Props) => {
   const loadThreshold = useRef(0);
-
-
   const [backgroundVideo, setBackgroundVideo] = useState<AssestType>(getRandomVideo());
+  const [nextVideo, setNextVideo] = useState<AssestType>(getRandomVideo());
 
-  const cachedTickFn = useCallback((
-    glitchFilter: GlitchEmisorFilter,
-    _deltacrtFilter: CRTFilter) => {
-    if (glitchFilter.intensity === 0.3) {
+  const cachedTickFn = (
+    filters: { glitch: GlitchEmisorFilter },
+    loading: boolean
+  ) => {
+    const { glitch } = filters;
+
+    if (loading) {
+      glitch.intensity = MAX_GLITCH_INDEX;
+      return;
+    }
+
+    // Check if user is glitching a lot
+    if (glitch.intensity >= MAX_GLITCH_INDEX) {
       loadThreshold.current++;
+
+      // User glitched a lot during some time
+      if (loadThreshold.current >= NEXT_VIDEO_THRESHOLD) {
+        loadThreshold.current = 0;
+        setBackgroundVideo(nextVideo)
+        setNextVideo(getRandomVideo())
+      }
     } else {
-      // reduce count over time
       loadThreshold.current = Math.max(loadThreshold.current - 1, 0);
     }
 
-    // user glitched a lot during some time
-    if (loadThreshold.current >= 20) {
-      loadThreshold.current = 0;
-      setBackgroundVideo(getRandomVideo())
-    }
-  }, [backgroundVideo]);
+    // Smooth glitch reduction over time
+    glitch.intensity = Math.max(glitch.intensity - GLITCH_DEGRADING_FACTOR, 0);
+  };
+
 
   return (
     <div className={`main-layout ${classProps}`}>
       <div className={`pixi-container`}>
-        <PixiBackground height={700} width={1400} videoData={backgroundVideo} ticker={cachedTickFn} asciiSize={1} />
+        <PixiBackground height={700} width={1400} videoData={backgroundVideo} nextVideoData={nextVideo} ticker={cachedTickFn} asciiSize={1} />
       </div>
       <div className={'main-layout-container'}>
         <NavigationBar />
