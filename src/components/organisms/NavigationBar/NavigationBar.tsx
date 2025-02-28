3446478f348819ebfaef62411e05b2079f92ee2c
Leonardo Julio Rios Aliaga <44624540+le4onardo@users.noreply.github.com>
2025-02-28 04:37:59 +0000

    New navigation bar

--- src/components/organisms/NavigationBar/NavigationBar.tsx
+++ src/components/organisms/NavigationBar/NavigationBar.tsx
@@ -1,53 +1,26 @@
 import './NavigationBar.css';
-import githubIcon from '../../../media/github-icon.png';
-import linkedinIcon from '../../../media/linkedin-icon.png';
-import cvIcon from '../../../media/new-cv-icon.png';
-import useGlitcher from '../../../hooks/useGlitcher';
+import Link from '../../atoms/Link';
+import { twMerge } from 'tailwind-merge';
 
-const NavigationBar = () => {
-  const cvGlitch = useGlitcher();
-  const githubGlitch = useGlitcher();
-  const linkedInGlitch = useGlitcher();
+interface Props {
+  className?: string
+}
 
+const NavigationBar = ({ className }: Props) => {
   return (
-    <div className='navigation-bar'>
-      
-      <div ref={cvGlitch.ref}>
-        <a
-          className={'home navigation-bar__link'}
-          href='https://docs.google.com/document/d/11ek2gR7W66aMLip-v5xD30T5KiThzfrACdcjJupOInk/edit?usp=sharing'
-          target='_blank'
-          rel='noopener noreferrer'
-          title='CV'
-        >
-          <img src={cvIcon} />
-          <div>CV</div>
-        </a>
-      </div>
-      <div ref={githubGlitch.ref}>
-        <a
-          href='https://github.com/le4onardo'
-          className='github navigation-bar__link'
-          rel='noopener noreferrer'
-          target='_blank'
-          title='GitHub'
-        >
-          <img src={githubIcon} />
-          <div>Github</div>
-        </a>
-      </div>
-      <div ref={linkedInGlitch.ref}>
-        <a
-          href='https://www.linkedin.com/in/leonardo-julio-rios-aliaga-017687122'
-          className='linkedin navigation-bar__link'
-          rel='noopener noreferrer'
-          target='_blank'
-          title='Linked In'
-        >
-          <img src={linkedinIcon} />
-          <div>LinkedIn</div>
-        </a>
-      </div>
+    <div className={twMerge('flex justify-between items-center gap-10 backdrop-blur-sm', className)}>
+      <Link href='#' className='mr-auto'>
+        <div className="">Home</div>
+      </Link>
+      <Link href='#AboutMe'>
+        <div className="">Skills</div>
+      </Link>
+      <Link href='#Skills'>
+        <div className="">About me</div>
+      </Link>
+      <Link href='#Blogs'>
+        <div className="">Blog</div>
+      </Link>
     </div>
   );
 };
