12b933e115fef3def0a8bf4869f666e324960f55
Leonardo Julio Rios Aliaga <44624540+le4onardo@users.noreply.github.com>
2025-03-30 19:22:02 +0000

    Fix text animator warning

--- src/components/molecules/TextAnimator.tsx
+++ src/components/molecules/TextAnimator.tsx
@@ -7,21 +7,36 @@ interface Props {
 
 }
 
+const WordAnimator = ({ text, charStyles }: Props) => {
+    return <div style={{ display: 'inline-block' }}>
+        {text.split('').map((char, index) => {
+            const style = typeof charStyles === 'function' ? charStyles(char, index) : charStyles;
+
+            return <p
+                key={`${char}-${index}`}
+                style={{ display: 'inline-block', position: 'relative', ...style }}
+            >
+                {char}
+            </p>
+        })}
+    </div>
+}
+
 export const TextAnimator = React.memo(({ text, charStyles = {}, className = '' }: Props) => {
-    // console.log('text animator', text);
+    const words = text.split(' ');
 
     return <div className={className} data-sentence={text}>
-        {text.split(' ').map((word, index) => {
-            const chars = word.split('').map(char => {
-                const style = typeof charStyles === 'function' ? charStyles(char, index) : charStyles;
-
-                return <p style={{ display: 'inline-block', position: 'relative', ...style }}>{char}</p>
-            });
-            return [
-                index > 0 ? <p style={{ display: "inline-block", whiteSpace: "pre-wrap" }}>{" "}</p> : null,
-                <div style={{ display: "inline-block" }} data-word={word}> {chars}</div>
-            ];
-        }).flat()
+        {words.map((word, index) => {
+            return <div key={`${word}-${index}`} style={{ display: "inline-block" }} data-word={word}>
+                <WordAnimator text={word} charStyles={charStyles} />
+                {
+                    index + 1 < words.length &&
+                    <p style={{ display: "inline-block", whiteSpace: "pre-wrap" }}>
+                        {" "}
+                    </p>
+                }
+            </div>;
+        })
         }
     </div>
 })
