bb8ff4a3328e8127201af18f24b265ebe8127d05
Leonardo Julio Rios Aliaga <44624540+le4onardo@users.noreply.github.com>
2025-04-11 04:46:17 +0000

    remove thick and add scroll detector

--- src/components/templates/MainLayout/MainLayout.tsx
+++ src/components/templates/MainLayout/MainLayout.tsx
@@ -11,65 +11,30 @@ interface Props {
   classProps: string;
 }
 
-const getRandomVideo = () => assets[Math.floor(Math.random() * assets.length)];
-const MAX_GLITCH_INDEX = 0.3
-const GLITCH_LOSS = 0.005
-const CRT_LOSS = 0.01
-const NEXT_VIDEO_THRESHOLD = 15
-
 const MainLayout: React.FC<Props> = ({ children, classProps }: Props) => {
-  const loadThreshold = useRef(0);
-  const [backgroundVideo, setBackgroundVideo] = useState<AssestType>(assets[20]);
-  const [nextVideo, setNextVideo] = useState<AssestType>(getRandomVideo());
+  const ref = useRef<HTMLDivElement>(null);
+  const [scroll, setScroll] = useState(true);
+  useEffect(() => {
+    const handleScroll = () => setScroll(window.scrollY > 0);
 
-  const cachedTickFn = (
-    filters: { glitch: GlitchEmisorFilter, crt: CRTEmisorFilter },
-    loading: boolean
-  ) => {
-    const { glitch, crt } = filters;
-
-    if (loading) {
-      // glitch.intensity = MAX_GLITCH_INDEX;
-      return;
-    }
-
-    // Check if user is glitching a lot
-    if (glitch.intensity >= MAX_GLITCH_INDEX) {
-      loadThreshold.current++;
-
-      // User glitched a lot during some time
-      if (loadThreshold.current >= NEXT_VIDEO_THRESHOLD) {
-        loadThreshold.current = 0;
-        setBackgroundVideo(nextVideo)
-        setNextVideo(getRandomVideo())
-      }
-    } else {
-      loadThreshold.current = Math.max(loadThreshold.current - 1, 0);
-    }
-
-    // Smooth glitch reduction over time
-    glitch.intensity = Math.max(glitch.intensity - GLITCH_LOSS, 0);
-    crt.intensity = Math.max(crt.intensity - CRT_LOSS, 0);
-  };
-
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <div className={`main-layout ${classProps}`}>
-      <div className={`pixi-container`}>
-        <link rel="" href={backgroundVideo.backgroundUrl} type="image/png" />
-        {
-          // <img style={{ height: 700, width: 1400 }} src={backgroundVideo.backgroundUrl}></img>
-          <>
-            <PixiBackground height={700} width={1400} videoData={backgroundVideo} nextVideoData={nextVideo} ticker={cachedTickFn} />
-          </>
-          // assets.map(videoData => <PixiBackground height={700} width={1400} videoData={videoData} />)
-        }
+    <>
+      <div className={`sticky z-10 top-0  border-b-neutral-700 backdrop-blur-sm ${scroll && "border-b-[1px]"}`} ref={ref}>
+        <NavigationBar className=' h-16 w-full mx-auto max-w-[1200px] px-12' />
+      </div >
+      <div className={`main-layout ${classProps}`}>
+
+        <div className={'main-layout-container'}>
+          {children}
+        </div>
       </div>
-      <div className={'main-layout-container'}>
-        <NavigationBar />
-        {children}
-      </div>
-    </div>
+    </>
   );
 };
 
