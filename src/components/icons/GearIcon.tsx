59d91c7bf03e2de237a6375af8d133e80dc66ba2
Leonardo Julio Rios Aliaga <44624540+le4onardo@users.noreply.github.com>
2025-04-16 04:15:49 +0000

    fix gear icon

--- src/components/icons/GearIcon.tsx
+++ src/components/icons/GearIcon.tsx
@@ -11,9 +11,9 @@ export default function GearIcon({ className = '', ...props }: Props) {
         height="24"
         viewBox="0 0 24 24"
         fill="none"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className={className}
         {...props}>
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
