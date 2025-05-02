e402141f0c8c4cac34c3e15ed00fc89ae5b2b1be
Leonardo Julio Rios Aliaga <44624540+le4onardo@users.noreply.github.com>
2025-05-02 02:40:39 +0000

    add loop support

--- src/components/icons/LordIcon.tsx
+++ src/components/icons/LordIcon.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, LegacyRef, RefObject, useEffect, useRef, useState } from 'react';
+import { ForwardedRef, forwardRef, LegacyRef, RefObject, useEffect, useRef, useState } from 'react';
 import { Player } from '@lordicon/react';
 import { IPlayerOptions } from '@lordicon/react/dist/interfaces';
 
@@ -6,6 +6,7 @@ import { IPlayerOptions } from '@lordicon/react/dist/interfaces';
 export interface LordIconProps extends Omit<IPlayerOptions, "icon"> {
     className?: string
     url: string
+    loop?: boolean
 }
 
 declare global {
@@ -17,8 +18,9 @@ declare global {
     }
 }
 
-function LordIcon({ url, className = '', ...props }: LordIconProps, ref: LegacyRef<Player>) {
+function LordIcon({ url, loop, className = '', ...props }: LordIconProps, ref: ForwardedRef<Player>) {
     const [iconData, setIconData] = useState<string | null>(null);
+    const localRef = useRef<Player | null>();
 
     useEffect(() => {
         fetch(url).then(
@@ -27,14 +29,18 @@ function LordIcon({ url, className = '', ...props }: LordIconProps, ref: LegacyR
     }, [url]);
 
 
-
-
     return (
         <div className={className}>
             {
                 !!iconData && <Player
-                    ref={ref}
+                    ref={(el) => {
+                        localRef.current = el;
+                        typeof ref === 'function' && ref(el)
+                    }}
                     icon={iconData}
+                    onComplete={() => {
+                        loop && localRef?.current?.playFromBeginning()
+                    }}
                     {...props}
                 />
             }
